2767a6772b5816c8f70dfd4934bc70c9bbe2a2e2
J Marza <joshua.marza@stcatz.ox.ac.uk>
2021-11-25 16:47:29 +0000

    Added negamax w/ alpha beta pruning and column order for large speed improvements

--- solver_algos.py
+++ solver_algos.py
@@ -36,7 +36,38 @@ def negamax(P: Position) -> int:
     return bestScore
 
 
-def negamaxpruning(P: Position) -> int:
-    """WORK IN PROGRESS: Negamax method but with pruning to discard certain 
+def negamaxpruning(P: Position, alpha: int, beta: int) -> int:
+    """Negamax method but with alpha-beta pruning to discard certain 
     branches for the sake of a faster runtime"""
-    return 0
+    col_order = [3,2,4,1,5,0,6]
+    b: int = P.height*P.width
+
+    # Returning 0 for a draw
+    if P.nbMoves == b: 
+        return 0
+    
+    # Returning score when there is a winning next move
+    for i in range(P.width):
+        if P.canPlay(i) and P.isWinningMove(i):
+            winning_score: int = (b+1-P.nbMoves)//2
+            return winning_score
+        
+    max: int = (b-1-P.nbMoves)//2;	# Upper bound to the score as we cannot win next move
+    if beta > max:
+        beta = max
+        if alpha >= beta:
+            return beta  # Prune if the alpha-beta interval is empty
+
+    for i in range(P.width):
+        if P.canPlay(col_order[i]):
+            P2 = deepcopy(Position(board=P.board))
+            P2.play(col_order[i])
+            score: int = -negamaxpruning(P2, -beta, -alpha)
+
+            if score >= beta:
+                return score
+
+            elif score > alpha:
+                alpha = score
+
+    return alpha
